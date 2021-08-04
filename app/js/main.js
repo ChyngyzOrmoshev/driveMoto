e1cbe88b9ab9f4a1a87ca476aa78ad5423be2d47
ChyngyzOrmoshev <chyngyzormoshev@gmail.com>
2021-08-04 19:48:12 +0600

    добавил блоки популярные товары и баннер скидки

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,10 @@
 $(function () {
+  // слайд заголовок
   $(".slider").slick({
     dots: true,
   });
 
+  // переключение поиск
   $(".search__number").click(function () {
     $(".search__number").addClass("search__filter-active");
     $(".search__brand").removeClass("search__filter-active");
@@ -22,6 +24,7 @@ $(function () {
     $("input").attr("placeholder", "Введите название товара");
   });
 
+  // hover эффект на категории
   $(".category__item").hover(
     function () {
       $(this).addClass("category__item-active");
@@ -31,9 +34,49 @@ $(function () {
     }
   );
 
+  // слайдер товаров
   $(".card").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     speed: 1000,
   });
+
+  // модальное окно
+  $(".card__form").click(function () {
+    $(".modal-popup").fadeIn();
+    $(".popup-overlay").fadeIn();
+  });
+  $(".popup-close").click(function () {
+    $(".modal-popup").fadeOut();
+    $(".popup-overlay").fadeOut();
+  });
+  $(".popup-overlay").click(function () {
+    $(".modal-popup").fadeOut();
+    $(".popup-overlay").fadeOut();
+  });
+  $(".modal-btn").click(function () {
+    alert(
+      "При поступлении товара на указанный e-mail отправим почту. Спасибо!"
+    );
+  });
+
+  // hover эффект на карточки
+  $(".card__item").hover(
+    function () {
+      $(this).addClass("card__hover");
+    },
+    function () {
+      $(this).removeClass("card__hover");
+    }
+  );
+  $(".card__content").hover(
+    function () {
+      $(this).prepend(
+        '<div class="card__hover-link"><a href="#">посмотреть товар</a></div>'
+      );
+    },
+    function () {
+      $(this).find($('.card__hover-link')).remove();
+    }
+  );
 });
